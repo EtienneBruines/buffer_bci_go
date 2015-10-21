b13fc6454db64f08e578148510f9d50b2058071f
Etienne Bruines <etienne@bruines.com>
2015-10-21 11:44:39 +0200

    Simple data acq client
    It now connects to the buffer server, and gets sample information.

---
+++ examples/connect/connect.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/EtienneBruines/go_buffer_bci_client/bufferbci"
+)
+
+func main() {
+	log.Println("Trying to connect to localhost:1972 ...")
+
+	conn, err := bufferbci.Connect("localhost:1972")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	log.Println("Connection established")
+
+	response, err := conn.SendDebugCommand()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Response:", response)
+}
